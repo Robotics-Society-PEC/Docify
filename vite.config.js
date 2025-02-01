3960be6b04f3f4e3353c6f481218d216e41b1044
Witty-Wizard <agarwalshashank429+github@gmail.com>
2025-02-01 18:35:57 +0530

    Added: Build Configs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        assetFileNames: 'index.css',
+        dir: './dist'
+      }
+    }
+  }
 });
